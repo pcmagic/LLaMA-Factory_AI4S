d3083158a35a0873de13fc5509585006922da951
Zhang Ji <gotofd@gmail.com>
2024-06-04 17:58:01 +0800

    show log info on screen and save to files at same time.

--- init_model.py
+++ init_model.py
@@ -1,11 +1,13 @@
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
+# current version inspired from google/gemma-2b
 tokenizer = AutoTokenizer.from_pretrained('google/gemma-2b')
 config = AutoConfig.from_pretrained('google/gemma-2b', vocab_size=len(tokenizer))
 config.hidden_activation = config.hidden_act
-# for gemma 1.3B
-# config.hidden_size = 1024
+# dbg code, generate a small checkpoint for dbg. 
+config.hidden_size = 128
+config.num_hidden_layers = 2
 model = AutoModelForCausalLM.from_config(config)
 
-model.save_pretrained('pretrain_ckp/gemma1.3/')
-tokenizer.save_pretrained('pretrain_ckp/gemma1.3/')
+model.save_pretrained('pretrain_ckp/dbg_gemma1.3/')
+tokenizer.save_pretrained('pretrain_ckp/dbg_gemma1.3/')
--- src/llamafactory/extras/logging.py
+++ src/llamafactory/extras/logging.py
@@ -49,12 +49,30 @@ def get_logger(name: str) -> logging.Logger:
     formatter = logging.Formatter(
         fmt="%(asctime)s - %(levelname)s - %(name)s - %(message)s", datefmt="%m/%d/%Y %H:%M:%S"
     )
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
+
+    # 创建一个handler，用于输出到控制台
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.INFO)
+
+    # 创建一个handler，用于写入日志文件
+    import os
+    logger_path = os.environ.get('OUTPUT_DIR', None)
+    logger_path = os.path.join(os.getcwd(), 'llamafactory_log') if logger_path is None \
+        else os.path.join(logger_path, 'llamafactory_log')
+    if not os.path.exists(logger_path):
+        os.makedirs(logger_path)
+    logger_name = os.path.join(logger_path, 
+                               f"{name}.log" if not name.endswith('.log') else name)
+    file_handler = logging.FileHandler(logger_name)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
 
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(handler)
+    # logger.setLevel(logging.INFO)  # set each log_handler separately. 
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+    logger.info("saving log info to file %s" % logger_name)
 
     return logger
 
